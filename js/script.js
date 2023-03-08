0df376417da6a46ca16e5791b7363f5aec8cabd2
BB pezsgő <bazsi5fel2@gmail.com>
2023-03-08 16:26:11 +0100

    Some progress again

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// @ts-nocheck
+window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('order-button').addEventListener('click', (e) => {
+        const pizzaType = document.getElementById('pizza-type').value
+        const pizzaCount = document.getElementById('pizza-count').value
+
+        if (!pizzaType || !pizzaCount || pizzaType === 'Válasszon egyet!' || pizzaCount === NaN) {
+            alert('Válasszon ki egy pizzát a listából és írja be a darabszámot!')
+            return
+        }
+
+        const pizzaPrice = {
+            "Nápolyi pizza": 3200,
+            "Bolognai pizza": 3000,
+            "Songoku pizza": 2800,
+            "Hawaii pizza": 2900
+        }[pizzaType] * pizzaCount
+
+        const orderID = Math.round(Math.random() * 100)
+
+        
+    })
+})
